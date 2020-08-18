c727cb070cc3cbb346ca9913fd3624ddcc077714
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-08-18 16:31:15 +0530

    ng g c

--- firstProject/src/app/app.module.ts
+++ firstProject/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { Header } from './header/head.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Header,
+    TestComponent,
   ],
   imports: [
     BrowserModule,
