7d91409082e61f593b69312f339c37136ce48c0d
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-09-28 15:48:53 +0530

    forms

--- firstProject/src/app/app.module.ts
+++ firstProject/src/app/app.module.ts
@@ -7,9 +7,11 @@ import { AppComponent } from "./app.component";
 import { Header } from "./header/head.component";
 import { HeadService } from "./header/header.service";
 import { TestComponent } from "./test/test.component";
+import { IoComponent, IoComponentView, IoComponentOut } from './io/io.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
-  declarations: [AppComponent, Header, TestComponent],
+  declarations: [AppComponent, Header, TestComponent, IoComponent, IoComponentView, IoComponentOut, FormComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [HeadService],
   bootstrap: [AppComponent],
