afba75ea2074d27e1e07eab0ccf9f19b7303188a
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-10-20 11:47:15 +0530

    initial heroku commit

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
+
 const express = require('express');
 const app = express();
-const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
 
+app.all('*', (req, res) => {
+    res.status(200).sendFile(__dirname + '/dist/index.html');
+});
+
 app.listen(process.env.PORT || 8080);
-
-//Path Location Strategy
-
-app.get('/*', function(req, res) {
-res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
-
-console.log('Console listening!');
